412eb79b9f2b8ce542152033a868415254c2b742
SerLegolas <e-ross1971@live.it>
2026-02-21 16:55:49 +0100

    Aggiunto meta viewport per navbar fissa in PWA/mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
         <script
