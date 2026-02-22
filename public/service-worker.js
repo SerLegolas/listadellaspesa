ce06f9c78d90b9162d8146d07f15b35d3b34e4c0
SerLegolas <e-ross1971@live.it>
2026-02-22 22:25:21 +0100

    Service worker aggiornato: cache versionata, update forzato PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,50 @@
+const CACHE_NAME = 'listaspesa-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/favicon.png',
+  '/icon.svg',
+  '/flaticon.svg',
+  // aggiungi altri file statici se necessario
+];
+
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
   clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        // Aggiorna cache con la nuova versione
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
